616fde8319555214eaf6e2ed14c675adb374b949
Kelsee Watts <watts.kelsee@gmail.com>
2023-08-03 13:38:14 -0500

    updated code should run up to challenge 5

--- Lab03_Review/Program.cs
+++ Lab03_Review/Program.cs
@@ -6,22 +6,59 @@ namespace Lab03_Review
     {
         public static void Main()
         {
-            CodeChallenge1();
-            CodeChallenge2();
-            CodeChallenge3();
-            CodeChallenge4();
+            Console.WriteLine("Welcome to the Code Challenges!");
+            Console.WriteLine("Choose a challenge to run:");
+            Console.WriteLine("1. CodeChallenge1");
+            Console.WriteLine("2. CodeChallenge2");
+            Console.WriteLine("3. CodeChallenge3");
+            Console.WriteLine("4. CodeChallenge4");
+            Console.WriteLine("5. CodeChallenge5");
+            Console.WriteLine("6. CodeChallenge6");
+            Console.Write("Enter the number of the challenge you want to run: ");
+
+            if (int.TryParse(Console.ReadLine(), out int choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        CodeChallenge1();
+                        break;
+                    case 2:
+                        CodeChallenge2();
+                        break;
+                    case 3:
+                        CodeChallenge3();
+                        break;
+                    case 4:
+                        CodeChallenge4();
+                        break;
+                    case 5:
+                        CodeChallenge5();
+                        break;
+                    case 6:
+                        CodeChallenge6();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please enter a number from 1 to 6.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
         }
         public static void CodeChallenge1()
         {
             Console.WriteLine("Please enter 3 numbers. Seperate each entry with a space");
-            string ? userInput = Console.ReadLine();
+            string? userInput = Console.ReadLine();
             int product = CalculatedProduct(userInput);
 
             Console.WriteLine($"The product of these 3 numbers is: {product}");
         }
 
         public static int CalculatedProduct(string userInput)
-          
+
         {
             string[] inputArray = userInput.Split(" ");
 
@@ -43,7 +80,7 @@ namespace Lab03_Review
                 }
                 else
                 {
-                    product *= 1; 
+                    product *= 1;
                 }
 
             }
@@ -64,7 +101,7 @@ namespace Lab03_Review
                 {
                     isValidInput = true;
                 }
-                else 
+                else
                 {
                     Console.WriteLine("Invalid input. Please try again.");
                 }
@@ -73,7 +110,7 @@ namespace Lab03_Review
             int sum = 0;
             int validInputs = 0;
 
-            for (int i =1; i <= number; i++)
+            for (int i = 1; i <= number; i++)
             {
                 Console.Write($"{i} of {number} - Enter a number: ");
                 if (int.TryParse(Console.ReadLine(), out int toBeAvgd) && toBeAvgd >= 0)
@@ -95,7 +132,7 @@ namespace Lab03_Review
 
         }
 
-        public static void CodeChallenge3() 
+        public static void CodeChallenge3()
         {
             Console.WriteLine("        *");
             Console.WriteLine("       ***");
@@ -108,7 +145,7 @@ namespace Lab03_Review
             Console.WriteLine("        *");
         }
 
-        public static void CodeChallenge4()
+        /*public static void CodeChallenge4()
         {
             Console.WriteLine("Please enter an array of numbers. Seperate each entry with a space");
             string ? inputNumbers = Console.ReadLine();
@@ -180,6 +217,59 @@ namespace Lab03_Review
             }
 
             return mostFrequentNode.Value;
+        }*/
+
+        static void CodeChallenge4()
+        {
+            int[] numbers = new int[]
+            { 1, 1, 2, 2, 3, 3, 3, 1, 1, 5, 5, 6, 7, 8, 2, 1, 1};
+            int mostSeen = 0;
+            int highScore = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];
+                int timesSeen = 1;
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] == currentNumber)
+                    {
+                        timesSeen++;
+                    }
+                }
+
+                if (timesSeen > highScore)
+                {
+                    mostSeen = currentNumber;
+                    highScore = timesSeen;
+                }
+            }
+            Console.WriteLine($"Most seen: {mostSeen}");
+        }
+
+        static void CodeChallenge5()
+        {
+            int highestNumber = 0;
+
+            int[] numbers = new int[] { 5, 25, 99, 123, 78, 96, 55, 188, 4 };
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if(numbers[i] > highestNumber)
+                {
+                    highestNumber = numbers[i];
+                }
+            }
+
+            Console.WriteLine($"This is the highest number: {highestNumber}");
+        }
+
+        static void CodeChallenge6()
+        {
+            Console.WriteLine("Enter a word: ");
+            string word = Console.ReadLine();
+            File.WriteAllText("C:\\Users\\raygr\\Documents\\Dev\\Sandbox\\Lab3\\words.txt", word);
         }
     }
 }
